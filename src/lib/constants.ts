f1ae736914be50ee998126f5364ce69b2e592c8d
juliomarttins <maretins10@gmail.com>
2025-11-20 22:34:42 -0300

    Adiciona arquivos faltantes: constants e transaction-form

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const APP_VERSION = "v.1.0.9";
+
+export const APP_NAME = "Poupp";
+
+export const DEFAULT_CATEGORIES = {
+    income: ['Salário', 'Bônus', 'Freelance', 'Investimentos', 'Rendimentos', 'Outros'],
+    expense: [
+        'Moradia', 'Alimentação', 'Transporte', 'Lazer', 'Saúde', 
+        'Educação', 'Mercado', 'Dívidas', 'Crianças', 'Assinaturas', 'Outros'
+    ]
+};
